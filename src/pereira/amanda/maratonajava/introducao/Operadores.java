b24fb2c7d38ac94818fc134c4e4ce560a2f73216
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-15 00:41:06 -0300

    Operadores lógicos

--- src/pereira/amanda/maratonajava/introducao/Operadores.java
+++ src/pereira/amanda/maratonajava/introducao/Operadores.java
@@ -9,9 +9,13 @@ public class Operadores {
         int subtracao = numero01 - numero02;
         int multiplicacao = numero01 * numero02;
         int divisao = numero01 / numero02;
-        String resultado = ("Soma é " + soma + " Subtração é: " + subtracao + " Multiplicação é: " + multiplicacao + " Divisão é: " + divisao);
+        int resto = numero01 % numero02;
 
+        String resultado = ("Soma é " + soma + " Subtração é: " + subtracao + " Multiplicação é: " + multiplicacao + " Divisão é: " + divisao + " O resto é: " + resto);
         System.out.println(resultado);
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        System.out.println("isDezMaiorQueVinte " +isDezMaiorQueVinte );
 
     }
 }
