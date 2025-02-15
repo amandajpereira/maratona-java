e3fdbb87bf653f3a9f5d9b56c09a2c4cd99a472a
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-14 23:48:03 -0300

    inserindo tipos

--- src/pereira/amanda/maratonajava/introducao/TiposPrimitivos.java
+++ src/pereira/amanda/maratonajava/introducao/TiposPrimitivos.java
@@ -4,5 +4,15 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         int idade = 29;
         System.out.println("A idade é: " + idade);
+        long numeroGrande = 1000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'F';
+
+
     }
 }
