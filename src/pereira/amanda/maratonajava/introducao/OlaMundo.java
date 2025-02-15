8931a668c6af835571fe5eb4876946fca796cacc
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-14 23:18:57 -0300

    tipos primitivos

--- src/pereira/amanda/maratonajava/introducao/OlaMundo.java
+++ src/pereira/amanda/maratonajava/introducao/OlaMundo.java
@@ -3,6 +3,5 @@ package pereira.amanda.maratonajava.introducao;
 public class OlaMundo {
     public static void main(String[] args) {
     System.out.printf("Olá Mundo!");
-
     }
 }
---
+++ src/pereira/amanda/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,8 @@
+package pereira.amanda.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 29;
+        System.out.println("A idade é: " + idade);
+    }
+}
