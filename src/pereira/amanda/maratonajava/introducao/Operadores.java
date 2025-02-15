185ad65010e11080c8c639777efb99090efeada0
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-15 00:27:22 -0300

    Operadores

---
+++ src/pereira/amanda/maratonajava/introducao/Operadores.java
@@ -0,0 +1,17 @@
+package pereira.amanda.maratonajava.introducao;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        int numero01 = 20;
+        int numero02 = 10;
+        int soma = numero01 + numero02;
+        int subtracao = numero01 - numero02;
+        int multiplicacao = numero01 * numero02;
+        int divisao = numero01 / numero02;
+        String resultado = ("Soma é " + soma + " Subtração é: " + subtracao + " Multiplicação é: " + multiplicacao + " Divisão é: " + divisao);
+
+        System.out.println(resultado);
+
+    }
+}
