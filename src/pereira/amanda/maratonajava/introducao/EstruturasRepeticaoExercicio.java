a63b6a7c0bc9ebccd4fbeb61c5d5aa85b0dfc57b
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-17 17:01:05 -0300

    Estruturas de repetição exercício - imprimir apenas pares

---
+++ src/pereira/amanda/maratonajava/introducao/EstruturasRepeticaoExercicio.java
@@ -0,0 +1,13 @@
+package pereira.amanda.maratonajava.introducao;
+
+public class EstruturasRepeticaoExercicio {
+    public static void main(String[] args) {
+        //Imprima todos os números pares de 0 até 1000
+
+        for(int i = 0; i <= 10; i++){
+            if( i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
