7522d91f79ea52a1bc29bdd80625e3603992a157
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-17 15:04:30 -0300

    Estruturas condicionais - else - else if

--- src/pereira/amanda/maratonajava/introducao/EstruturasCondicionais.java
+++ src/pereira/amanda/maratonajava/introducao/EstruturasCondicionais.java
@@ -3,7 +3,8 @@ package pereira.amanda.maratonajava.introducao;
 public class EstruturasCondicionais {
     public static void main(String[] args) {
 
-        int idade = 17;
+        int idade = 30;
+        String categoria;
         boolean isAutorizadoComprarBebida = idade >= 18;
 
         if (isAutorizadoComprarBebida) {
@@ -13,6 +14,21 @@ public class EstruturasCondicionais {
         if (!isAutorizadoComprarBebida) {
             System.out.println("Não autorizado a comprar bebida alcólica");
         }
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcólica");
+        } else {
+            System.out.println("Não autorizado a comprar bebida alcólica");
+        }
+
+        if (idade < 15) {
+            categoria = "Infantil";
+        } else if (idade >= 15 && idade < 18) {
+            categoria = "Juvenil";
+        } else {
+            categoria = "Adulto";
+        }
+        System.out.println(categoria);
 
     }
 }
