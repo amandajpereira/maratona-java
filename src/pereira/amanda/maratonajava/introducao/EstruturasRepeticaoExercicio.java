db971891a6007aaaa2775346e6d5479aeb3cb79b
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-17 17:37:30 -0300

    Estruturas de repetição - incluindo break

--- src/pereira/amanda/maratonajava/introducao/EstruturasRepeticaoExercicio.java
+++ src/pereira/amanda/maratonajava/introducao/EstruturasRepeticaoExercicio.java
@@ -1,5 +1,7 @@
 package pereira.amanda.maratonajava.introducao;
 
+import java.sql.SQLOutput;
+
 public class EstruturasRepeticaoExercicio {
     public static void main(String[] args) {
         //Imprima todos os números pares de 0 até 1000
@@ -17,5 +19,19 @@ public class EstruturasRepeticaoExercicio {
             }
 
         }
+
+        //Dado o valor de um carro descubra quantas vezes ele pode ser parcelado
+        //Valor da parcela deve ser maior ou igual a mil
+        double valorTotal = 30000;
+
+        for(int parcela = 1; parcela <= valorTotal; parcela++){
+            double valorParcela = valorTotal / parcela;
+            if(valorParcela < 1000){
+                break;
+            }
+            System.out.println("Parcela " + parcela + "R$ " + valorParcela);
+        }
+
+
     }
 }
