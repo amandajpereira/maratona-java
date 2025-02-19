9b72d2097f3df47c4ad9214f58b8c65f2d6e383e
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-19 11:42:24 -0300

    Arrays multidimensionais

--- src/pereira/amanda/maratonajava/introducao/ArraysMultidimesionais.java
+++ src/pereira/amanda/maratonajava/introducao/ArraysMultidimesionais.java
@@ -1,4 +1,26 @@
 package pereira.amanda.maratonajava.introducao;
 
 public class ArraysMultidimesionais {
+    public static void main(String[] args) {
+
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int i = 0; i < matriz.length; i++) { // Percorre as linhas
+            for (int j = 0; j < matriz[i].length; j++) { // Percorre as colunas
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(); // Pula para a próxima linha
+        }
+
+        for(int[] arrBase : matriz ) {
+            for (int num : arrBase) {
+                System.out.println(num);
+            }
+        }
+
+    }
 }
