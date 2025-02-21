ea71f1e799e4e2fe1ee789bc30a1bb13b333fcd9
Amanda Pereira <estudosjpereira.amanda@gmail.com>
2025-02-21 15:41:58 -0300

    Orientação a Objetos - Referência de Objetos

--- src/pereira/amanda/maratonajava/javacore/Aintroducaoclasses/test/AnimalTest01.java
+++ src/pereira/amanda/maratonajava/javacore/Aintroducaoclasses/test/AnimalTest01.java
@@ -6,15 +6,33 @@ public class AnimalTest01 {
     public static void main(String[] args) {
 
         Animal animal = new Animal();
+        Animal animal2 = new Animal();
 
         animal.tipo = "Cachorro";
         animal.nome = "Kiara";
         animal.idade = 1;
         animal.sexo = 'F';
-
         System.out.println(animal.tipo);
         System.out.println(animal.nome);
         System.out.println(animal.idade);
         System.out.println(animal.sexo);
+
+        animal2.tipo = "Cachorro";
+        animal2.nome = "Betho";
+        animal2.idade = 10;
+        animal2.sexo = 'M';
+
+        System.out.println(animal2.tipo);
+        System.out.println(animal2.nome);
+        System.out.println(animal2.idade);
+        System.out.println(animal2.sexo);
+
+        animal = animal2;
+        System.out.println(animal.nome);
+        System.out.println(animal.tipo);
+        System.out.println(animal.nome);
+        System.out.println(animal.idade);
+        System.out.println(animal.sexo);
+
     }
 }
